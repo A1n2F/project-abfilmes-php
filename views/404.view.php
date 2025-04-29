60741821377a0f396794831846c7c95772e69677
A1n2F <alissonfferreira22@gmail.com>
2025-04-29 12:36:31 -0300

    Criando o roteador

---
+++ views/404.view.php
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center">
+    <div class="flex flex-col items-center">    
+        <h1 class="text-xl text-white">Página não encontrada...</h1>
+        <p class="text-2xl text-white">:(</p>
+
+        <a href="/" class="mt-4 hover:text-purple-500">Voltar para home</a>
+    </div>
+</div>
