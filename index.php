a1e6f597fe4fa585fc33197f86b92fa3b3d9ef0a
A1n2F <alissonfferreira22@gmail.com>
2025-04-29 10:15:58 -0300

    Criando o card de um filme

--- index.php
+++ index.php
@@ -44,9 +44,27 @@
             </div>
         </form>
 
-        <div>
-            lista final
-        </div>
+        <section class="space-y-4">
+            <div class="border-2 border-gray-300 w-[280px] h-[360px] rounded-xl p-5 relative">
+                <div class="absolute bottom-5">
+                    <h1 class="text-xl text-gray-200 font-semibold">Titulo do filme</h1>
+                    <span class="flex gap-2">
+                        <p class="text-gray-300">Gênero</p>
+                        <p>.</p>
+                        <p class="text-gray-300">Ano</p>
+                    </span>
+                </div>
+
+                <div class="absolute right-2 top-2">
+                    <span class="bg-black/70 rounded-full px-2.5 py-2 flex gap-1 items-center">
+                        <h1 class="text-xl text-white font-bold">4,5</h1>
+                        <p>/</p>
+                        <h1 class="text-sm">5</h1>
+                        <img src="/assets/star.svg" alt="">
+                    </span>
+                </div>
+            </div>
+        </section>
     </main>
 </body>
 </html>
