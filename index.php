56de4a49757452a93803edde0dcf6c3722de41b7
A1n2F <alissonfferreira22@gmail.com>
2025-04-29 10:56:14 -0300

    criação de um superglobal

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+require 'dados.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -44,26 +50,32 @@
             </div>
         </form>
 
-        <section class="space-y-4">
-            <div class="border-2 border-gray-300 w-[280px] h-[360px] rounded-xl p-5 relative">
-                <div class="absolute bottom-5">
-                    <h1 class="text-xl text-gray-200 font-semibold">Titulo do filme</h1>
-                    <span class="flex gap-2">
-                        <p class="text-gray-300">Gênero</p>
-                        <p>.</p>
-                        <p class="text-gray-300">Ano</p>
-                    </span>
-                </div>
+        <section class="grid grid-cols-5 gap-6">
 
-                <div class="absolute right-2 top-2">
-                    <span class="bg-black/70 rounded-full px-2.5 py-2 flex gap-1 items-center">
-                        <h1 class="text-xl text-white font-bold">4,5</h1>
-                        <p>/</p>
-                        <h1 class="text-sm">5</h1>
-                        <img src="/assets/star.svg" alt="">
-                    </span>
+            <?php foreach($filmes as $filme): ?>
+                <div class="border-2 border-gray-300 w-[280px] h-[360px] rounded-xl p-5 relative">
+                    <div class="absolute bottom-5">
+                        <a href="/filme.php?id=<?=$filme['id']?>" class="text-xl text-gray-200 font-semibold hover:text-purple-600">
+                            <?=$filme['titulo']?>
+                        </a>
+                        <span class="flex gap-2">
+                            <p class="text-gray-300"><?=$filme['genero']?></p>
+                            <p>.</p>
+                            <p class="text-gray-300"><?=$filme['ano']?></p>
+                        </span>
+                    </div>
+
+                    <div class="absolute right-2 top-2">
+                        <span class="bg-black/70 rounded-full px-2.5 py-2 flex gap-1 items-center">
+                            <h1 class="text-xl text-white font-bold">4,5</h1>
+                            <p>/</p>
+                            <h1 class="text-sm">5</h1>
+                            <img src="/assets/star.svg" alt="">
+                        </span>
+                    </div>
                 </div>
-            </div>
+            <?php endforeach; ?>
+            
         </section>
     </main>
 </body>
